9ff190a07fb9f78e2b96a15b9b11ce2cf15b5416
a-d-iii <adi.xv0@gmail.com>
2025-06-27 00:32:32 +0530

    Add ExperimentalFoundationApi opt-in

--- app/src/main/java/com/example/basic/ui/HomePager.kt
+++ app/src/main/java/com/example/basic/ui/HomePager.kt
@@ -6,10 +6,12 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 
 /** Simple pager replacing fragment_home.xml's ViewPager2 */
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun HomePager(items: List<String>) {
     val pagerState = rememberPagerState(pageCount = { items.size })
