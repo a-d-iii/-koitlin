0c5e6d00dfd0de494ec7543e3446af68dcace316
a-d-iii <adi.xv0@gmail.com>
2025-06-29 17:59:21 +0530

    Fix missing background import

---
+++ app/src/main/java/com/example/basic/ui/theme/Color.kt
@@ -0,0 +1,12 @@
+package com.example.basic.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val mdPrimary = Color(0xFF6750A4)
+val mdSecondary = Color(0xFF625B71)
+val mdBackground = Color(0xFFF6F6F6)
+val mdSurface = Color.White
+val mdOnPrimary = Color.White
+val mdOnSecondary = Color.White
+val mdOnBackground = Color(0xFF1C1B1F)
+val mdOnSurface = Color(0xFF1C1B1F)
